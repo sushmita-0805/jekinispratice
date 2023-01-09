38fc455460a9329a6009b9dbd4ec50e76c967835
Anup Sarkar <Anup Sarkar@DESKTOP-L9U6IEH>
2023-01-09 16:25:33 +0530

    Jekinis TestCase

---
+++ jenikines/src/test/java/demo/test1.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+
+	public void m1()
+	{
+		Reporter.log("hi",true);
+	}
+}
---
+++ jenikines/src/test/java/demo/test2.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void m2()
+	{
+		Reporter.log("hello",true);
+	}
+	
+
+}
